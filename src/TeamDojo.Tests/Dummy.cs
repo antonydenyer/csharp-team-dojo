9d0e3d4cd238ad43c7f0a43c405c8e7a37030226
Antony Denyer <antonydenyer@gmail.com>
2012-06-12 11:40:51 +0100

    added dummy template project

---
+++ src/TeamDojo.Tests/Dummy.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TeamDojo.Tests
+{
+	[TestFixture]
+	public class when_building_using_a_ci_server
+	{
+		[Test]
+		public void then_the_test_should_be_green()
+		{
+			Assert.True(true);
+		}
+	}
+}
